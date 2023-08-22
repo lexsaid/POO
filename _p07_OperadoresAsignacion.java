ab81f5ab12f3e8cc5323ae9c65c6515573805950
lexsaid <talli_stq@hotmail.com>
2023-08-22 09:23:23 -0600

    actividad 2

--- _p07_OperadoresAsignacion.java
+++ _p07_OperadoresAsignacion.java
@@ -13,9 +13,9 @@ public class _p07_OperadoresAsignacion {
         out.printf("incrementar: %.2f \n", ++num);
         out.printf("sumar 80: %.2f \n",num+=80);
         out.printf("restar 35: %.2f \n",num-=35);
-        out.printf("mult x15: %.2f \n",num);
-        out.printf("div /4 es: %.2f \n",num);
-        out.printf("decrementar: %.2f \n",num);
+        out.printf("mult x15: %.2f \n",num*=15);
+        out.printf("div /4 es: %.2f \n",num/=4);
+        out.printf("decrementar: %.2f \n", --num);
 
     }
 }
